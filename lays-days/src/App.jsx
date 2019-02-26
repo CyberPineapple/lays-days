d3f44ac4a5b0ccaf761e5f7d9bb13a0db3d71a8e
Cyber_pineapple <cyber_pineapple@mail.ru>
2019-02-26 23:33:37 +0300

    save

--- lays-days/src/App.jsx
+++ lays-days/src/App.jsx
@@ -4,7 +4,7 @@ export default class App extends React.Component {
   render() {
     return (
         <div className="app">
-            <div className="header">
+            <div className="navbar">
                 <div className="menu">
                     <div className="menu-logo">
                         <div className="menu-logo-icon"></div>
@@ -12,15 +12,15 @@ export default class App extends React.Component {
                     </div>
                     <div className="menu-button-layout">
                         <div className="menu-button">
-                            <div className="menu-button-icon-1"></div>
+                            <div className="icon-1"></div>
                             <p className="menu-button-text">Галерея</p>
                         </div>
                         <div className="menu-button">
-                            <div className="menu-button-icon-2"></div>
+                            <div className="icon-2"></div>
                             <p className="menu-button-text">О проекте</p>
                         </div>
                         <div className="menu-button">
-                            <div className="menu-button-icon-3"></div>
+                            <div className="icon-3"></div>
                             <p className="menu-button-text">Лето с Lay's</p>
                         </div>
                     </div>
@@ -28,7 +28,13 @@ export default class App extends React.Component {
                 </div>
             </div>
             <div className="content">
-                
+                <div className="animation-left">
+                    <div className="content-section-1"></div>
+                </div>
+                <div className="content-section-2">
+                    <div className="1"></div>
+                    <div className="2"></div>
+                </div>
             </div>
       </div>
     );
