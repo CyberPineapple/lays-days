35201498b817de05fd5b9d45c50c1358cf1285de
Cyber_pineapple <cyber_pineapple@mail.ru>
2019-03-03 21:38:00 +0300

    save

---
+++ lays-days/src/components/content.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+// import content from './conten.module.css';
+
+export default class Content extends React.Component{
+
+
+    render() {
+        let images = [
+            <div style={{"background-image": "url(https://pepsico-lays-yellow-trend.snpdev.ru/images/stub/1.dc59205a.png)", "background-size": "cover"}}></div>,
+            <div style={{"background-image": "url(https://pepsico-lays-yellow-trend.snpdev.ru/images/stub/2.09d5f843.png)", "background-size": "cover"}}></div>,
+            <div style={{"background-image": "url(https://pepsico-lays-yellow-trend.snpdev.ru/images/stub/3.416330f5.png)", "background-size": "cover"}}></div>,
+            <div style={{"background-image": "url(https://pepsico-lays-yellow-trend.snpdev.ru/images/stub/4.71d17a4f.png)", "background-size": "cover"}}></div>,
+            <div style={{"background-image": "url(https://pepsico-lays-yellow-trend.snpdev.ru/images/stub/5.d0f1531e.png)", "background-size": "cover"}}></div>,
+            <div style={{"background-image": "url(https://pepsico-lays-yellow-trend.snpdev.ru/images/stub/6.d542dd6f.png)", "background-size": "cover"}}></div>,
+            <div style={{"background-image": "url(https://pepsico-lays-yellow-trend.snpdev.ru/images/stub/7.7c6c40b0.png)", "background-size": "cover"}}></div>,
+            <div style={{"background-image": "url(https://pepsico-lays-yellow-trend.snpdev.ru/images/stub/8.0a2ab336.png)", "background-size": "cover"}}></div>,
+            <div style={{"background-image": "url(https://pepsico-lays-yellow-trend.snpdev.ru/images/stub/9.4c9c4447.png)", "background-size": "cover"}}></div>,
+        ]
+
+        return (
+            <div className="content">
+                <div className="animation-left">
+                    <div className="content-section-1"></div>
+                </div>
+                <div className="content-section-2">
+                    <div className="content-section-2-1"></div>
+                    <div className="content-section-2-2">
+                        {images}
+                    </div>
+                    <div className="content-section-3"></div>
+                </div>
+            </div>
+        )
+    }
+}
